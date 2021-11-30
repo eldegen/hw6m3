306efdeeaae6511779af1198fbd1546a423a39c8
eldegen <eliyaz2761@gmail.com>
2021-11-30 18:43:39 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw6m3/MainFragment.java
@@ -0,0 +1,31 @@
+package com.example.hw6m3;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hw6m3.databinding.FragmentMainBinding;
+
+public class MainFragment extends Fragment {
+    private FragmentMainBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/example/hw6m3/SecondFragment.java
@@ -0,0 +1,23 @@
+package com.example.hw6m3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hw6m3.databinding.FragmentSecondBinding;
+
+public class SecondFragment extends Fragment {
+    private FragmentSecondBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
